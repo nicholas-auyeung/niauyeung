0d67c26b0eb7370cad376feb3710d8286becca61
Nicholas <niauyeung@gmail.com>
2022-03-23 16:39:51 -0700

    app created

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Header from '../src/components/Header/Header'
+
+const Home = () => {
+  return(
+    <Header/>
+  );
+};
+
+export default Home;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+import {AiFillGithub} from 'react-icons/ai';
+
+const Header = () => (
+    <div>
+        <AiFillGithub size="3rem"/>
+    </div>
+
+);
+
+export default Header;
